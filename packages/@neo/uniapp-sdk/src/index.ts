ee9a9eff51cbd94399678c6a0e895bba27e12b56
R3E Developer <dev@r3e.network>
2026-01-27 08:30:10 +0800

    fix(sdk): remove components export and include useI18n in build

--- packages/@neo/uniapp-sdk/src/index.ts
+++ packages/@neo/uniapp-sdk/src/index.ts
@@ -4,4 +4,4 @@ export * from "./composables";
 export { mockSDK, installMockSDK } from "./mock";
 export * from "./card-types";
 export * from "./card-mock";
-export * from "./components";
+// Note: components are Vue SFCs, import directly from @neo/uniapp-sdk/components if needed
